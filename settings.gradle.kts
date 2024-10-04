d002c3007b87841ad9e38fc7b92e2b162f8ef667
pambrose <pambrose@mac.com>
2024-10-03 23:57:58 -0700

    Consolidate kdocs into a single build dir

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,16 @@
+pluginManagement {
+  repositories {
+    maven(url = "./maven-repo")
+    gradlePluginPortal()
+  }
+}
+
 plugins {
   id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
+
 rootProject.name = "vapi4k"
+
 include("vapi4k-core")
 include("vapi4k-dbms")
 include("vapi4k-utils")
